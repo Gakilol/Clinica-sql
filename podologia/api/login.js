d4c90d0e222dc5d532ae646477351dc4d251c1cc
Gakilol <kevingabrield23@gmail.com>
2025-11-23 12:51:09 -0600

    Update login.js

--- podologia/api/login.js
+++ podologia/api/login.js
@@ -3,12 +3,12 @@ export default function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Método no permitido" });
   }
-  const { usuario, contrasena } = req.body;
+  const { usuario, contraseña } = req.body;
 
-  if (usuario === "admin" && contrasena === "Admin123*") {
+  if (usuario === "admin" && contraseña === "Admin123*") {
     return res.json({ mensaje: "Login exitoso", rol: "admin", usuario: "admin" });
   }
-  if (usuario === "cajero" && contrasena === "Cajero123*") {
+  if (usuario === "cajero" && contraseña === "Cajero123*") {
     return res.json({ mensaje: "Login exitoso", rol: "cajero", usuario: "cajero" });
   }
   return res.status(401).json({ error: "Credenciales incorrectas" });
